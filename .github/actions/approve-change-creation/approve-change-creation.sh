b7fa43f343f83146382672874fa23eb0b71ca45b
Hung Vu <vunguyenhung@outlook.com>
2024-09-24 13:33:45 +0700

    Improve actions

--- .github/actions/approve-change-creation/approve-change-creation.sh
+++ .github/actions/approve-change-creation/approve-change-creation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 echo '{
   "status": "success",
   "message": "Change Approval ticket created successfully",
--- .github/actions/code-scan/code-scan.sh
+++ .github/actions/code-scan/code-scan.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 echo '{
   "status": "success",
   "message": "Code scan completed successfully",
